820fd736e0947bbc3ed2adb11b555d1bab1274d0
UtkarshKr007 <utkarshkumar2021@gmail.com>
2023-07-12 16:05:43 -0500

    Create Gamma Card with ongoing effect and picture

---
+++ Assets/Scripts/Card/Cards/GammaCard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GammaCard: Card {
+    
+    public override void Awake() {
+        cardName = "Gamma";
+        base.Awake();
+    }
+    
+     public override void onGoing() {
+        int oppositeCardsCount = getOpposingCards().Count;
+        PowerUtils.Modifier addXPower = new PowerUtils.Modifier("Gamma", "Add", oppositeCardsCount);
+        addPowerMod(addXPower);
+    }
+
+}
