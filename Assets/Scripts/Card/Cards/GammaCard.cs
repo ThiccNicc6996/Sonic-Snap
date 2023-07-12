e98002de55af6ae52f183eb49c9e556ef153d708
ThiccNicc6996 <nicolas.gamefreak.robertson@gmail.com>
2023-07-12 17:15:03 -0500

    Added Gamma Script to Gamma prefab

--- Assets/Scripts/Card/Cards/GammaCard.cs
+++ Assets/Scripts/Card/Cards/GammaCard.cs
@@ -11,7 +11,10 @@ public class GammaCard: Card {
     
      public override void onGoing() {
         int oppositeCardsCount = getOpposingCards().Count;
+        List<Card> opposingCards = getOpposingCards();
+
         PowerUtils.Modifier addXPower = new PowerUtils.Modifier("Gamma", "Add", oppositeCardsCount);
+
         addPowerMod(addXPower);
     }
 
